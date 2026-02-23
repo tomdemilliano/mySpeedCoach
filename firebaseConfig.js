03f2b3092765510f8e157088e3ef8452ca613d41
tomdemilliano <tomdemilliano@gmail.com>
2026-02-23 14:41:18 +0100

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBh7YT9hGp4S2GTEzsfsCBkuEUMdK6OdaA",
+  authDomain: "myspeedcoach-416ac.firebaseapp.com",
+  projectId: "myspeedcoach-416ac",
+  storageBucket: "myspeedcoach-416ac.firebasestorage.app",
+  messagingSenderId: "637458200530",
+  appId: "1:637458200530:web:77c17fa5cb39f0b3333a0b",
+  measurementId: "G-3XZDT4QFX4"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
