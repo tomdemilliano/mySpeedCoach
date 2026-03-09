e1068245e9e35e612a3fe947c1e06eb0109e9759
tomdemilliano <tomdemilliano@gmail.com>
2026-03-09 17:33:24 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="manifest" href="../public/manifest.json" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#0f172a" />
         <meta name="mobile-web-app-capable" content="yes" />
         <link rel="icon" href="/icons/icon-192.png" />
