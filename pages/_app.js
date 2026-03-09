b4f3c4172742ecdce12b85bc61891ba7dfe5ec3c
tomdemilliano <tomdemilliano@gmail.com>
2026-03-09 17:17:10 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import AppLayout from '../components/AppLayout';
+import Head from 'next/head';
 //import '../styles/globals.css'; // keep if you already have this; remove if not
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <AppLayout>
-      <Component {...pageProps} />
-    </AppLayout>
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0f172a" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <link rel="icon" href="/icons/icon-192.png" />
+      </Head>
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </>
   );
 }
