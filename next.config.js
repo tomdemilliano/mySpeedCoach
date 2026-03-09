047922f1b6df6ea24c22931dce43b26646b5ac8e
tomdemilliano <tomdemilliano@gmail.com>
2026-03-09 17:32:56 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {},
+};
 
-module.exports = withPWA({
-  // your existing next config here
-});
+module.exports = nextConfig;
