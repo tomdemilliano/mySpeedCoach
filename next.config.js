66d687cbea8c715458a97fdd099c574091e5f3a8
tomdemilliano <tomdemilliano@gmail.com>
2026-03-11 16:53:56 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  // Explicitly exclude API routes from the service worker
-  navigateFallbackDenylist: [/^\/api\//],
-  runtimeCaching: [],
-});
+const nextConfig = {
+  turbopack: {},
+};
 
-const nextConfig = {};
-
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
