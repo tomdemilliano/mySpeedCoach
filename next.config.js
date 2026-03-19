498b356638a203ae931f5cc35bd324bba26b134a
tomdemilliano <tomdemilliano@gmail.com>
2026-03-20 00:07:09 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  turbopack: {},
-};
+// next.config.js
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: false,          // we register manually in the hook
+  skipWaiting: true,
+  // Tell next-pwa not to overwrite our custom sw.js
+  swSrc: 'public/sw.js',   // use ours as the source
+});
 
-module.exports = nextConfig;
+module.exports = withPWA({ turbopack: {} });
