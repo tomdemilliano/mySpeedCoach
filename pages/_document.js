c27485a7ac058d058ab7cc704ff1aa327890b188
tomdemilliano <tomdemilliano@gmail.com>
2026-04-02 16:34:20 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="nl">
-      <Head />
+      <Head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <link rel="apple-touch-startup-image" href="/icons/skippr-logo-transparant.png" />
+      </Head>
       <body style={{ margin: 0, padding: 0, backgroundColor: '#0f172a' }}>
         <Main />
         <NextScript />
