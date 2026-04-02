bd79cd049e354f5660f8d5a95c20b59e46a42e7b
tomdemilliano <tomdemilliano@gmail.com>
2026-04-02 15:35:49 +0200

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="nl">
+      <Head />
+      <body style={{ margin: 0, padding: 0, backgroundColor: '#0f172a' }}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
