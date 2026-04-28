39377e60a5131e79d7c187680594a6d334a5ce2b
tomdemilliano <tomdemilliano@gmail.com>
2026-04-28 14:32:39 +0200

    Add meta tags and apple touch icon to document

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,8 @@ export default function Document() {
       <Head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Skippr" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
         <link rel="apple-touch-startup-image" href="/icons/skippr-logo-transparant.png" />
       </Head>
       <body style={{ margin: 0, padding: 0, backgroundColor: '#0f172a' }}>
